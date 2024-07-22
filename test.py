3ef3580828657eb9eaf9cff0f757e49f10f07cdc
Stanislas07 <stanislas.akilitoa07@gmail.com>
2024-07-23 10:56:14 +1100

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ def on_button_click():
     frm = ttk.Frame(root, padding=10)
     
 
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Label(frm, text="Vous avez réussi !").grid(column=0, row=0)
 ttk.Button(frm, texte="Click me", commande = on_button_click).grid(column=1, row=0)
 
 root.mainloop()
